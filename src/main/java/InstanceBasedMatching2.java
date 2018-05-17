199f8a6c6afe14f8acbc938f06aafe501b8e5b80
arianeziehn <arianeziehn@googlemail.com>
2018-05-17 18:26:21 +0200

    final update

--- src/main/java/InstanceBasedMatching2.java
+++ src/main/java/InstanceBasedMatching2.java
@@ -1,22 +1,19 @@
 import uk.ac.shef.wit.simmetrics.similaritymetrics.Levenshtein;
 import uk.ac.shef.wit.simmetrics.similaritymetrics.MongeElkan;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 /*
-*
-*
-*
-*
- */
+* Instance-based matching
+*/
 
 public class InstanceBasedMatching2 {
-
+    // holding the schemas
     private static List<String> schemaSource = new ArrayList<String>();
     private static List<String> schemaTarget = new ArrayList<>();
+    // Hash Map for 1) both complete sets, 2) for a reduced set
     private static HashMap imdb = new HashMap<String, HashSet<Integer>>();
     private static HashMap rottenTomatoes = new HashMap<String, HashSet<Integer>>();
     private static HashMap imdb1 = new HashMap<String, HashSet<Integer>>();
@@ -27,23 +24,26 @@ public class InstanceBasedMatching2 {
     public static void main(String[] args) {
 
         long timeStart = System.currentTimeMillis();
+        //small sets
         String sourcePath = "C:\\Users\\Ariane\\git\\Gorilla\\src\\main\\resources\\randomImdbres.csv";
         String targetPath = "C:\\Users\\Ariane\\git\\Gorilla\\src\\main\\resources\\randomRTres.csv";
+        imdb = readData(sourcePath,true);
+        rottenTomatoes = readDataBig(targetPath, false);
+        // System.out.println(imdb.size()); 21458 entries
+        // System.out.println(rottenTomatoes.size()); 8067 entries
+
+        // big sets
         String sourcePath1 = "C:\\Users\\Ariane\\git\\Gorilla\\src\\main\\resources\\imdbtest3.csv";
         String targetPath1 = "C:\\Users\\Ariane\\git\\Gorilla\\src\\main\\resources\\rotten_tomatoestest3.csv";
         imdb1 = readDataBig(sourcePath1,true);
         rottenTomatoes1 = readData(targetPath1, false);
-        imdb = readData(sourcePath,true);
-        rottenTomatoes = readDataBig(targetPath, false);
-
-        // System.out.println(imdb.size()); 21458 entries
-        // System.out.println(rottenTomatoes.size()); 8067 entries
 
         HashMap match = new HashMap<Integer, HashMap<Integer,Integer>>( );
         HashMap finalMatch = new HashMap<Integer, HashMap<Integer,Integer>>( );
         List<Integer> assignColRT = new ArrayList<>();
         List<Integer> assignColImdb = new ArrayList<>();
 
+        //------- Phase 1 ---- exact matches of big sets
         Iterator keySetImbd = imdb1.keySet().iterator( );
         while (keySetImbd.hasNext()) {
             String imdbKey = keySetImbd.next( ).toString( );
@@ -66,9 +66,8 @@ public class InstanceBasedMatching2 {
                 }
             }
         }
-
-        for (int columncounterImdb = 0; columncounterImdb <= maxColumnSource; columncounterImdb++) {
-
+        // extract knowledge base
+        for (int columncounterImdb = 0; columncounterImdb < maxColumnSource; columncounterImdb++) {
             if (match.containsKey(columncounterImdb)) {
                 HashMap<Integer, Integer> aMatch = (HashMap<Integer, Integer>) match.get(columncounterImdb);
                 if (aMatch.size( ) == 1) {
@@ -84,8 +83,8 @@ public class InstanceBasedMatching2 {
         System.out.println(" Already assign colImdb: " + assignColImdb);
         System.out.println(" Already assign colRT: " + assignColRT);
 
-
-        for (int columncounterImdb = 0; columncounterImdb <= maxColumnSource; columncounterImdb++) {
+        // second phase
+        for (int columncounterImdb = 0; columncounterImdb < maxColumnSource; columncounterImdb++) {
 
             if (!assignColImdb.contains(columncounterImdb)) {
                 float resultME = 0f;
@@ -95,33 +94,33 @@ public class InstanceBasedMatching2 {
                 double simFinalL = 0.0;
                 int colCounterME = 0;
                 int colCounterL = 0;
+                boolean colFinished = false;
 
                 Iterator keySetImdb = imdb.keySet( ).iterator( );
                 // here we check for each word of the dictionary
-                while (keySetImdb.hasNext( )) {
-                    //current MapEntry
+                while (keySetImdb.hasNext( ) && !colFinished) {
+                    //current MapEntry imdb
                     String pairImdb = keySetImdb.next( ).toString( );
 
                     if (imdb.get(pairImdb).toString( ).contains(columncounterImdb + "")) {
-                        //System.out.println("GOT IT"+imdb.get(pairImdb).toString()+ ","+(columncounterImdb+""));
 
-                        for (int columncounterRt = 0; columncounterRt <= maxColumnTarget; columncounterRt++) {
+                        for (int columncounterRt = 1; columncounterRt < maxColumnTarget; columncounterRt++) {
+
                             if (!assignColRT.contains(columncounterRt)) {
                                 Iterator keySetRt = rottenTomatoes.keySet( ).iterator( );
                                 while (keySetRt.hasNext( )) {
+                                    //current MapEntry rt
+                                    String pairRt = keySetRt.next().toString();
 
-                                    String pairRt = keySetRt.next( ).toString( );
-
-                                    if (rottenTomatoes.get(pairRt).toString( ).contains(columncounterRt + "")) {
-
+                                    if (rottenTomatoes.get(pairRt).toString().contains(columncounterRt + "")) {
                                         MongeElkan mE = new MongeElkan( );
                                         Levenshtein metricL = new Levenshtein( );
                                         resultME += mE.getSimilarity(pairImdb, pairRt);
                                         resultL += metricL.getSimilarity(pairImdb, pairRt);
                                         count++;
                                     }
-
                                 }
+
                                 double simME = resultME / (double) count;
                                 double simL = resultL / (double) count;
 
@@ -134,54 +133,54 @@ public class InstanceBasedMatching2 {
                                     colCounterL = columncounterRt;
                                 }
 
+                                if(count > 100000){
+
+                                    if(count > 100000 && simFinalL >= 0.11){
+                                        HashMap<Integer, Integer> result = new HashMap<Integer, Integer>( );
+                                        result.put(colCounterL, 0);
+                                        assignColRT.add(colCounterL);
+                                        assignColImdb.add(columncounterImdb);
+                                        finalMatch.put(columncounterImdb, result);
+                                        colFinished = true;
+                                    }
+                                    if(count > 100000 && simFinalL < 0.11){
+                                        // no match for this col
+                                        colFinished = true;
+                                    }
+
+                                    break;
+                                }
                             }
-                            long currentTime2 = System.currentTimeMillis();
-                            System.out.println("----FINISHED col " + columncounterRt + " for " +columncounterImdb +" in " + (currentTime2-timeStart));
+                            //System.out.println("----FINISHED colRT " + columncounterRt + " for col Imdb " +columncounterImdb );
+                            //System.out.println("----SimME for Col " + colCounterME + " with " + simFinalME + " SimL for Col " + colCounterL + " with " +simFinalL);
                         }
-
                     }// for coloumnRT
-
                 }
 
                 HashMap<Integer, Integer> result = new HashMap<Integer, Integer>( );
                 System.out.println("------------" + columncounterImdb + "--------------");
                 System.out.println("simME " + simFinalME + ", " + colCounterME);
                 System.out.println("simL " + simFinalL + ", " + colCounterL);
-                long currentTime = System.currentTimeMillis();
-                if (simFinalME > 0.5 && simFinalL > 0.25) {
-                    if(simFinalME > simFinalL) {
-                        result.put(colCounterME, 0);
-                        assignColRT.add(colCounterME);
-                        assignColImdb.add(columncounterImdb);
-                        finalMatch.put(columncounterImdb, result);
-                    }
-                    else{
+
+                if (simFinalME > 0.21 && simFinalL > 0.15) {
+
                         result.put(colCounterL, 0);
                         assignColRT.add(colCounterL);
                         assignColImdb.add(columncounterImdb);
                         finalMatch.put(columncounterImdb, result);
-                    }
+                        colFinished = true;
 
                 }
-
-                System.out.println("The final match : " + finalMatch);
-
+                //System.out.println("The final match : " + finalMatch);
             }
             long currentTime = System.currentTimeMillis();
-            System.out.println("----FINISHED col " + columncounterImdb + "in " + (currentTime-timeStart));
+            System.out.println("----FINISHED col Imdb " + columncounterImdb + " time so far : " + ((currentTime-timeStart)/1000) + "s");
         }
         long timeEnd = System.currentTimeMillis();
-        System.out.println(finalMatch);
-        System.out.println(timeEnd-timeStart);
-        /*
-        current output for small sets from Henrik && it takes very long for colimdb 6, the rest is relativly fast
-        besseres umgehen mit den sim werten
-        wanted result with test2 datasets: (1 id) 2->2 (name), 3->3 (year), 4->4 (releaseDate),5->5 (Director),6->6(Creator)
-        7->8(cast), 8->11(duration), 9->12(ratingvalue), 13->17 (description)
-        !!! Achtung: Hier entfällt immer col 0 in 4 for schleifen, weil da noch eine Zeilennummer ist
-         */
 
-    }
+        System.out.println(finalMatch);
+        System.out.println("TOTAL TIME " + (timeEnd-timeStart)/1000 + "s");
+       }
 
 
     private static HashMap readData(String filepath, boolean source){
@@ -199,7 +198,6 @@ public class InstanceBasedMatching2 {
                 if(!header) {
                     for (int counter = 0; counter < split.length; counter++) {
                         String attribute = split[counter].trim().toLowerCase().replace('"', ' ').replaceAll(" ", "").replaceAll("_", "").replaceAll("-","");
-
 
                         if(!attribute.equals("") && !attribute.equals(" ")) {
                             if(attribute.contains(",")){
@@ -249,18 +247,6 @@ public class InstanceBasedMatching2 {
                         String attribute = split[counter].trim().toLowerCase().replace('"', ' ').replaceAll(" ", "").replaceAll("_", "").replaceAll("-","");
 
                         if(!attribute.equals("") && !attribute.equals(" ")) {
-                            /*if(attribute.contains(",")){
-                                String[] split2 = attribute.split(",");
-                                for (int counter2 = 0; counter2 < split2.length; counter2++) {
-                                    String att = split2[counter2].trim( ).toLowerCase( ).replace('"', ' ').replaceAll(" ", "").replaceAll("_", "").replaceAll("-", "");
-                                    if(!att.equals("") && !att.equals(" ")) {
-                                        HashSet<Integer> aMatch = (HashSet<Integer>) dataBase.getOrDefault(att, new HashSet<>( ));
-                                        aMatch.add(counter);
-                                        dataBase.put(att, aMatch);
-                                    }
-                                }
-                            }
-                            else{*/
                             HashSet<Integer> aMatch = (HashSet<Integer>) dataBase.getOrDefault(attribute, new HashSet<>());
                             aMatch.add(counter);
                             dataBase.put(attribute, aMatch);
@@ -285,8 +271,8 @@ public class InstanceBasedMatching2 {
         boolean source = schemaSource.isEmpty();
         if(source) maxColumnSource = split.length;
         else maxColumnTarget = split.length;
-        System.out.println(maxColumnSource);
-        System.out.println(maxColumnTarget);
+        //System.out.println(maxColumnSource);
+        //System.out.println(maxColumnTarget);
         for(int i = 0; i < split.length; i++){
             // first cleaning:
             // lower case and remove empty fields
