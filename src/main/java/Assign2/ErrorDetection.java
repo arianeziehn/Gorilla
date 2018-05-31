f361b904e98b2bd8c48873e06d1d68d66bde3314
arianeziehn <arianeziehn@googlemail.com>
2018-05-31 17:11:08 +0200

    Error Detection

--- src/main/java/Assign2/ErrorDetection.java
+++ src/main/java/Assign2/ErrorDetection.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ErrorDetection {
 
     private static List<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
+    static int numberOfCol = 0;
 
     public static void main(String[] args) {
 
@@ -29,16 +31,17 @@ public class ErrorDetection {
         try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
 
             String sCurrentLine;
-            int rowCount = 0;
-            while ((sCurrentLine = br.readLine()) != null) {
-                ArrayList<String> row = new ArrayList();
-                String[] split = sCurrentLine.replace(" ","").split(",");
-                System.out.println(sCurrentLine);
-                System.out.println(split.length);
-                if(!header) {
-                    for (int counter = 0; counter < split.length; counter++) {
-                        String attribute = split[counter].trim().replace('"', ' ').replaceAll(" ", "");
+            int rowCount = 1;
 
+            while ((sCurrentLine = br.readLine()) != null) {
+                ArrayList<String> row = new ArrayList(12);
+                String[] split = sCurrentLine.replace(", ", ";").replace(" ","").split(",");
+
+                if(!header) {
+                    for (int counter = 0; counter < numberOfCol; counter++) {
+                        String attribute = "";
+                        if(counter < split.length) attribute = split[counter].trim().replace('"', ' ').replaceAll(" ", "");
+                        else row.add(counter, "E");
                         if(!attribute.equals("") && !attribute.equals(" ") && !attribute.matches("[a-z]+")) {
 
                                 row.add(counter, attribute);
@@ -48,18 +51,22 @@ public class ErrorDetection {
                             row.add(counter, "E");
                         }
                     }
-                    System.out.println(rowCount+ " :" + row);
-                    if(!(row.get(6).length()==2)) row.set(6,"E");
+                    //state
+                    if(!(row.get(6).length()==2) && !row.get(6).matches("[A-Z]*")){
+                        row.set(6,"E");
+                    }
                     // ZIP
                     if(!(row.get(7).length()==5)) row.set(7,"E");
+                    //ssn
                     if(!(row.get(10).length()>=8 && row.get(10).length()<11 )) row.set(10,"E");
 
                     input.add(rowCount, row);
-                    System.out.println(rowCount+ " :" + row);
+
                     rowCount++;
                 }
                 else{
                     header = false;
+                    createSchema(split);
                 }
 
             }
@@ -70,4 +77,25 @@ public class ErrorDetection {
 
 
     }
+
+    private static void createSchema(String[] split) {
+
+        numberOfCol = split.length;
+        ArrayList<String> row = new ArrayList();
+
+        for(int i = 0; i < split.length; i++) {
+            // first cleaning:
+            // lower case and remove empty fields
+            String attribute = split[i].trim( ).toLowerCase( ).replace('"', ' ').replace(" ", "");
+            row.add(i,attribute);
+        }
+        //System.out.println(row.get(6));
+        //System.out.println(row.get(7));
+        //System.out.println(row.get(10));
+
+                input.add(0,row);
+
+        }
+
+
 }
